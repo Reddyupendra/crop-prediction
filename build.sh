f17103831d2530029a0bb26cd2d71855ea59c273
Reddyupendra <reddyupendra02@gmail.com>
2026-04-14 17:09:26 +0530

    Fixed missing media directory model file

--- build.sh
+++ build.sh
@@ -4,5 +4,10 @@ set -o errexit
 
 pip install -r requirment.txt
 
+# Ensure media folder exists and model is in it
+mkdir -p media
+cp crop_model.pkl media/
+
 python manage.py collectstatic --no-input
+
 python manage.py migrate
